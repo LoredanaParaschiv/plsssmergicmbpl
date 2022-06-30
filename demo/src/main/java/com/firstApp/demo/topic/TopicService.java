3f0fc14723eea1a142bccc0784d47c1dd4cc6383
LoredanaParaschiv <paraschivloredana11@gmail.com>
2022-06-30 14:10:45 +0300

    Initial commit

---
+++ demo/src/main/java/com/firstApp/demo/topic/TopicService.java
@@ -0,0 +1,38 @@
+package com.firstApp.demo.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+
+    public List<Topic> getAllTopics() {
+        List<Topic> topicList = new ArrayList<>();
+        topicRepository.findAll().forEach(topicList::add);
+        return topicList;
+    }
+
+    public Topic getTopic(String id) {
+       return topicRepository.findById(id).get();
+    }
+
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updateTopic(Topic topic) {
+        topicRepository.save(topic);
+        }
+
+
+    public void deleteTopic(String id) {
+        topicRepository.deleteById(id);
+    }
+}
